8ce8d4b23e9b773edf2ae6fbf93dce9f2584cbf8
Inchara <incharap.cy24@rvce.edu.in>
2025-05-30 05:41:38 +0530

    Remove market prices and farming assistant features, add disease detection

---
+++ src/app/disease-detection/page.tsx
@@ -0,0 +1,11 @@
+import DiseaseDetection from "@/components/DiseaseDetection";
+import { BackButton } from "@/components/BackButton";
+
+export default function DiseaseDetectionPage() {
+  return (
+    <div>
+      <BackButton />
+      <DiseaseDetection />
+    </div>
+  );
+}
